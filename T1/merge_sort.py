80422f40ed5e0f0e6c347b38feb92e0d3c78bb50
stefanovp <stefano.viper@gmail.com>
2022-04-09 19:59:38 -0300

    made mergesort a class

--- T1/merge_sort.py
+++ T1/merge_sort.py
@@ -1,45 +1,46 @@
-import math
+class MergeSort():
 
-def merge(arr, begin : int, mid : int, end : int):
-    i = begin
-    j = mid + 1
-    k = begin
-    result = arr[:]
-    while i <= mid and j <= end:
-        if arr[i] <= arr[j]:
+    def merge(arr, begin : int, mid : int, end : int):
+        i = begin
+        j = mid + 1
+        k = begin
+        result = arr[:]
+        while i <= mid and j <= end:
+            if arr[i] <= arr[j]:
+                result[k] = arr[i]
+                i += 1
+                k += 1
+            else:
+                result[k] = arr[j]
+                j += 1
+                k += 1
+        
+        while i <= mid:
             result[k] = arr[i]
             i += 1
             k += 1
-        else:
+        while j <= end:
             result[k] = arr[j]
             j += 1
-            k += 1
-    
-    while i <= mid:
-        result[k] = arr[i]
-        i += 1
-        k += 1
-    while j <= end:
-        result[k] = arr[j]
-        j += 1
-        k += 1  
+            k += 1  
 
-    arr[:] = result
+        arr[:] = result
 
-def merge_sort(arr, begin : int, end : int):
-    if(begin >= end):
-        return
-    
-    mid = (begin + end)//2
-    merge_sort(arr, begin, mid)
-    merge_sort(arr, mid + 1, end)
-    merge(arr, begin, mid, end)
+    def sort(arr, begin : int, end : int):
+        if(begin >= end):
+            return
+        
+        mid = (begin + end)//2
+        MergeSort.sort(arr, begin, mid)
+        MergeSort.sort(arr, mid + 1, end)
+        MergeSort.merge(arr, begin, mid, end)
 
 def main():
-    arr = [12, 11, 13, 5, 6, 7]
+    arr = [13,64,3423,767,979,34,123,4,66]
     print("Before sort:", end="\n")
     print(arr)
-    merge_sort(arr, 0, len(arr) - 1)
+    MergeSort.sort(arr, 0, len(arr) - 1)
+    print("Before sort:", end="\n")
     print(arr)
 
 main()
