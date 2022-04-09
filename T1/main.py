bed154e5e6b7bd37c1dadfa9de4bd6c81b70e2c3
stefanovp <stefano.viper@gmail.com>
2022-04-09 20:21:48 -0300

    added insertion sort

--- T1/main.py
+++ T1/main.py
@@ -1,5 +1,6 @@
 import time
 
+from insertion_sort import InsertionSort
 from merge_sort import MergeSort
 from file_utils import AAFileUtils
 
@@ -9,20 +10,34 @@ def instrumented_merge_sort(dataset_data):
         stop = time.time()
         return stop - start
 
+def instrumented_insertion_sort(dataset_data):
+        start = time.time()
+        InsertionSort.sort(dataset_data)
+        stop = time.time()
+        return stop - start
+
 def main():
     
-    # Load data
+    # Load data into memory
     test_datasets = AAFileUtils.load_test_subjects("T1/test_data/DataSortB.txt")
     print('Test data successfully loaded')
     print('Test Datasets:')
     for set_name in test_datasets:
         print(f"dataset {set_name} ({len(test_datasets[set_name])} lines)")
+    print()
     
     # Run sorts
+    # ATENÇÂO: estamos sortando os datasets inplace, então somente um tipo
+    # de sort abaixo pode estar descomentado. Caso contrario, tentaremos
+    # sortaer dados já sorteados. 
+    # ps ctrl + k + c para comment varias linhas, ctrl + k + u para uncomment
     for dataset_name in test_datasets:
         dataset_data = test_datasets[dataset_name]
         
-        elapsed = instrumented_merge_sort(dataset_data)
+        elapsed = instrumented_insertion_sort(dataset_data)
         print(f"Sorted {dataset_name}. ElapsedTime = {elapsed}s")
+
+        # elapsed = instrumented_merge_sort(dataset_data)
+        # print(f"Sorted {dataset_name}. ElapsedTime = {elapsed}s")
 
 main()
